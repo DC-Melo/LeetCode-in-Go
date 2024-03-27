a96f3b7498348c27e41959bbee4d748f3730e1b2
DC <dc@sjyangdeMacBook-Air.local>
2024-03-27 22:46:19 +0800

    c

--- channel0goroutine/main.go
+++ channel0goroutine/main.go
@@ -7,11 +7,18 @@ import (
 
 func main() {
 	ch := make(chan struct{})
+	a := 2
+	b := 2
 	go func() {
 		fmt.Println("begin")
 		time.Sleep(time.Second * 3)
 		ch <- struct{}{}
 	}()
+	a++
+	a += b
+
+	fmt.Println(b)
+	fmt.Println(a)
 	fmt.Println("main process")
 	<-ch
 	fmt.Println("finished")
